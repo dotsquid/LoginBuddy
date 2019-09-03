b295766d584ea9e7ff854438772821a4e36575f8
dotsquid <dotsquid@gmail.com>
2019-09-03 18:06:00 +0300

    Add controller for password visibility button

---
+++ Assets/Scripts/PasswordVisibilityController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PasswordVisibilityController : MonoBehaviour
+{
+    [SerializeField]
+    private Image _image;
+    [SerializeField]
+    private Sprite _onSprite;
+    [SerializeField]
+    private Sprite _offSprite;
+
+    private bool _isOn;
+
+    public void OnClick()
+    {
+        _isOn = !_isOn;
+        _image.sprite = _isOn
+                      ? _onSprite
+                      : _offSprite;
+    }
+}
