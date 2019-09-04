0b731e189d6e58fcefc02484c20b159cdcaaa02a
dotsquid <dotsquid@gmail.com>
2019-09-04 17:36:35 +0300

    Toggle password visibility when a corresponding button is pressed

--- Assets/Scripts/PasswordVisibilityController.cs
+++ Assets/Scripts/PasswordVisibilityController.cs
@@ -1,22 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PasswordVisibilityController : MonoBehaviour
 {
+    public event Action<bool> onChange;
+
     [SerializeField]
     private Image _image;
     [SerializeField]
     private Sprite _onSprite;
     [SerializeField]
     private Sprite _offSprite;
+    [SerializeField]
+    private TMP_InputField _passwordField;
 
     private bool _isOn;
 
     public void OnClick()
     {
         _isOn = !_isOn;
-        _image.sprite = _isOn
-                      ? _onSprite
-                      : _offSprite;
+        if (_isOn)
+        {
+            _image.sprite = _onSprite;
+            _passwordField.inputType = TMP_InputField.InputType.Standard;
+        }
+        else
+        {
+            _image.sprite = _offSprite;
+            _passwordField.inputType = TMP_InputField.InputType.Password;
+        }
+        _passwordField.ForceLabelUpdate();
+        onChange?.Invoke(_isOn);
     }
 }
